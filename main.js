0e9183ea41e9b3b73f63d213f2b47ed5260d13e5
Varannchik <varann@inbox.ru>
2019-05-16 10:48:55 +0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
-let a=prompt('Введите число', 0);
+let b=prompt('Введите число', 0);
+    a= +b;
 if (a<0) {
     console.log('а отрицательное число');
   } else if (a==0) {
@@ -9,7 +10,7 @@ if (a<0) {
     console.log(a);
   }
 
-  let   fruits = ["Клубника", "Яблоко", "Апельсин", "Груша", "Слива"];
+  let   fruits = ["Клубника", "Яблоко", "Апельсин", "Груша", "Слива"],
         numbers = [2, 4, 6, 8, 10];
 console.log(fruits);
 console.log(numbers);
