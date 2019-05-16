9b8a3f5f50d7f57c8c5beba9a35f35d45446a195
Varannchik <varann@inbox.ru>
2019-05-16 10:37:53 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,15 @@
+let a=prompt('Введите число', 0);
+if (a<0) {
+    console.log('а отрицательное число');
+  } else if (a==0) {
+    console.log('а=0');
+  } else if (isNaN(a)) {
+    console.log('Это не число');
+  } else{
+    console.log(a);
+  }
+
+  let   fruits = ["Клубника", "Яблоко", "Апельсин", "Груша", "Слива"];
+        numbers = [2, 4, 6, 8, 10];
+console.log(fruits);
+console.log(numbers);
